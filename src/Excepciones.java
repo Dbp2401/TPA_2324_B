6200c766e0510cf11fddb7de2d386446afece39c
Carlos Mestre <carmesce@gmail.com>
2023-10-09 15:06:38 +0200

    Excepciones manejadas

--- src/Excepciones.java
+++ src/Excepciones.java
@@ -9,40 +9,78 @@ import java.io.IOException;
 
 public class Excepciones {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException
-    {
-        // ArithmeticException
-        // Lanzada cuando ocurre una excepción aritmética, como la división por cero.
+    public static void main(String[] args) {
 
-        int result = 10 / 0;
-        System.out.println("Resultado: " + result);
+        // ArithmeticException`
+        // Lanzada cuando ocurre una excepción aritmética, como la división por cero.
+        try {
+            int result = 10 / 0;
+            System.out.println("Resultado: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir por cero.");
+            System.out.println("Mensaje: " + e.getMessage());
+            System.out.println("ToString: " + e.toString());
+
+        }
 
         // NullPointerException
         // Lanzada cuando se intenta acceder a un método o propiedad de un objeto nulo.
-
         String str = null;
-        int length = str.length();
-        System.out.println("Longitud de la cadena: " + length);
+        try {
+            int length = str.length();
+            System.out.println("Longitud de la cadena: " + length);
+        } catch (NullPointerException e) {
+            System.out.println("El objeto es nulo.");
+            System.out.println("Mensaje: " + e.getMessage());
+            System.out.println("ToString: " + e.toString());
+        }
 
         // ArrayIndexOutOfBoundsException
         // Lanzada cuando se intenta acceder a un índice de arreglo que no existe.
         int[] array = {1, 2, 3};
-        int value = array[5];
-        System.out.println("Valor: " + value);
+        try {
+            int value = array[5];
+            System.out.println("Valor: " + value);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Índice del arreglo fuera de límites.");
+            System.out.println("Mensaje: " + e.getMessage());
+            System.out.println("ToString: " + e.toString());
+        }
 
         // NumberFormatException
         // Lanzada cuando intentamos convertir una cadena en un tipo numérico, pero la cadena no tiene el formato correcto.
-        int number = Integer.parseInt("XYZ");
-        System.out.println("Número: " + number);
+        try {
+            int number = Integer.parseInt("XYZ");
+            System.out.println("Número: " + number);
+        } catch (NumberFormatException e) {
+            System.out.println("Formato numérico inválido.");
+            System.out.println("Mensaje: " + e.getMessage());
+            System.out.println("ToString: " + e.toString());
+        }
 
         // FileNotFoundException
-        // Lanzada cuando no se encuentra un archivo especificado. Es importante notar que esta excepción es verificada, por lo que es necesario manejarla o declararla en la firma del método con `throws`.        
-        Scanner scanner = new Scanner(new File("archivo_inexistente.txt"));
+        // Lanzada cuando no se encuentra un archivo especificado. Es importante notar que esta excepción es verificada, por lo que es necesario manejarla o declararla en la firma del método con `throws`.
+        try {
+            Scanner scanner = new Scanner(new File("archivo_inexistente.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado.");
+            System.out.println("Mensaje: " + e.getMessage());
+            System.out.println("ToString: " + e.toString());
+        }
 
         // IOException
-        // Una excepción general para errores de entrada/salida. También es una excepción verificada.        
-        BufferedReader reader = new BufferedReader(new FileReader("somefile.txt"));
-        String line = reader.readLine();
-        System.out.println("Línea leída: " + line);
+        // Una excepción general para errores de entrada/salida. También es una excepción verificada.
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("somefile.txt"));
+            String line = reader.readLine();
+            System.out.println("Línea leída: " + line);
+        } catch (IOException e) {
+            System.out.println("Error de IO: " + e.getMessage());
+            System.out.println("Mensaje: " + e.getMessage());
+            System.out.println("ToString: " + e.toString());
+            System.out.println("printStackTrace:");
+            e.printStackTrace(System.out);
+        }
     }
 }
+*/
